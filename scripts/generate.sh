d0792fd17909639a343a3e583f36e1c2ba04534a
Giancarlos Salas <giansalex@gmail.com>
2022-05-06 11:51:46 -0500

    Update script path

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -13,6 +13,6 @@ protoc \
     --plugin="./node_modules/.bin/protoc-gen-ts_proto" \
     --ts_proto_out="${OUT_DIR}" \
     --proto_path="${PROTO_PATH}" \
-    --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=true" \
+    --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=messages,useDate=false" \
     ${x}
 done
